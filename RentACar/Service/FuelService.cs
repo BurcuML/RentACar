7238395e9e6bda59514c5069ba3d6d0508165dad
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-09-23 09:26:08 +0300

    Update FuelService.cs

--- RentACar/Service/FuelService.cs
+++ RentACar/Service/FuelService.cs
@@ -10,49 +10,49 @@ namespace RentACar.Service
 {
     internal class FuelService
     {
-        private CarData carData = new CarData();
-        public void Add(Car car)
+        private FuelData fuelData = new FuelData();
+        public void Add(Fuel fuel)
         {
-            carData.Add(car);
+           fuelData.Add(fuel);
         }
         public void GetById(int id)
         {
-            Car? car = carData.GetById(id);
-            if (car is null)
+            Fuel? fuel = fuelData.GetById(id);
+            if (fuel is null)
             {
-                Console.WriteLine($"Aradığınız Id ye göre ARABA bulunamadı :{id}");
+                Console.WriteLine($"Aradığınız Id ye göre Fuel bulunamadı :{id}");
                 return;
             }
-            Console.WriteLine($"Seçilen ARABA id si: {id}");
+            Console.WriteLine($"Seçilen id: {id}");
             Console.WriteLine(car);
         }
         public void Delete(int id)
         {
-            Car? car = carData.Delete(id);
-            if (car is null)
+            Fuel? fuel = fuelData.Delete(id);
+            if (fuel is null)
             {
-                Console.WriteLine($"ARABA Bulunamadı : Id= {id}");
+                Console.WriteLine($"Bulunamadı : Id= {id}");
                 return;
             }
-            Console.WriteLine("ARABA  Silindi.");
-            Console.WriteLine(car);
+            Console.WriteLine("Silindi.");
+            Console.WriteLine(fuel);
         }
         public void GetAll()
         {
-            List<Car> cars = carData.GetAllCars();
-            cars.ForEach(x => Console.WriteLine(x));
+            List<Fuel> fuels = fuelData.GetAll();
+            fuels.ForEach(x => Console.WriteLine(x));
         }
 
-        public void Update(int id, Car updatedCar)
+        public void Update(int id, Car uFuel)
         {
-            Car? car = carData.Update(id, updatedCar);
-            if (car is null)
+            Fuel? fuel = fuelData.Update(id, updatedCar);
+            if (fuel is null)
             {
-                Console.WriteLine($"Güncellemek istediğiniz ARABA bulunamadı: Id= {id}");
+                Console.WriteLine($"Güncellemek istediğiniz bilgi bulunamadı: Id= {id}");
                 return;
             }
-            Console.WriteLine("ARABA güncellendi.");
-            Console.WriteLine(car);
+            Console.WriteLine("Güncellendi.");
+            Console.WriteLine(fuel);
         }
     }
 }
