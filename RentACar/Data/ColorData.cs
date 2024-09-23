bf8b2a2cb518d1d026e53359db3b852cfe3605b8
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-09-23 08:15:31 +0300

    Update ColorData.cs

--- RentACar/Data/ColorData.cs
+++ RentACar/Data/ColorData.cs
@@ -8,5 +8,57 @@ namespace RentACar.Data
 {
     internal class ColorData
     {
+        private List<Color> colors = new List<Color>()
+{
+        new Color(),
+};
+
+        public Color Add(Color color)
+        {
+            colors.Add(color);
+            return color;
+        }
+
+        public List<Color> GetAllCars() {
+            return color; }
+
+        public Color GetById(int id)
+        {
+            Color? color = colors.SingleOrDefault(x => x.Id == id);
+            return color;
+        }
+
+        public Color Delete(int id)
+        {
+            Color? color = GetById(id);
+
+            if (car is not null)
+            {
+                colors.Remove(color);
+            }
+            else
+            {
+                return null;
+            }
+
+            return color;
+
+        }
+
+        public Color Update(int id)
+        {
+            Color? color = GetById(id);
+
+            if (color is not null)
+            {
+                color = new Color(
+    id: updatedCar.Id,
+    colorName: updateColorName.ColorId,
+    );
+            }
+
+            return color;
+
+        }       
     }
 }
