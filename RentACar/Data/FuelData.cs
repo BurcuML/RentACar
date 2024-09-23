d3498a69796b2038e3c003d823215090fbcad340
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-09-23 09:09:59 +0300

    Update FuelData.cs

--- RentACar/Data/FuelData.cs
+++ RentACar/Data/FuelData.cs
@@ -6,7 +6,69 @@ using System.Threading.Tasks;
 
 namespace RentACar.Data
 {
-    internal class FuelData
+    public class FuelData
     {
+        private List<Fuel> fuels = new List<Fuel>()
+{
+        new Fuel(),
+};
+
+        public Fuel Add(Fuel car)
+        {
+            fuels.Add(fuel);
+            return fuel;
+        }
+
+        public List<Fuel> GetAllCars() {
+            return fuels; }
+
+        public Fuel GetById(int id)
+        {
+            Fuel? fuel = fuels.SingleOrDefault(x => x.Id == id);
+            return fuel;
+        }
+
+        public Fuel Delete(int id)
+        {
+            Fuel? fuel = GetById(id);
+
+            if (fuel is not null)
+            {
+                fuels.Remove(fuel);
+            }
+            else
+            {
+                return null;
+            }
+
+            return fuel;
+
+        }
+
+        public Fuel Update(int id, Fuel uFuel)
+        {
+            Fuel? fuel = GetById(id);
+
+            if (fuel is not null)
+            {
+                fuel = new Fuel(
+    id: uFuel.Id,
+    colorId: uFuel.ColorId,
+    fuelId: uFuel.FuelId,
+    transmissionId: uFuel.TransmissionId,
+    carState: uFuel.CarState,
+    kiloMeter: uFuel.KiloMeter,
+    modelYear: uFuel.ModelYear,
+    plate: uFuel.Plate,
+    brandName: uFuel.BrandName,
+    modelName: uFuel.ModelName,
+    modelName: uFuel.ModelName,
+    dailyPrice: uFuel.DailyPrice
+    );
+            }
+
+            return fuel;
+
+        }
     }
 }
